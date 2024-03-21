131ac58741cdbef09d3000b0d2d785a7b45dbcc9
juhua111 <64871605+juhua111@users.noreply.github.com>
2024-03-21 22:27:07 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,5 +7,5 @@ co.no_imgs(True).mute(True)
 page = ChromiumPage(co)
 page.get("https://www.baidu.com")
 title = page.ele('tag:title').text
-print(title)
+print(title.encode('utf-8').decode())
 page.quit()
