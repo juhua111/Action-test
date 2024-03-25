49189473609721fa964f16cb7854aec710245803
juhua111 <3268561524@qq.com>
2024-03-25 13:11:24 +0800

    Delete test2.py

--- test2.py
+++
@@ -1,7 +0,0 @@
-from DrissionPage import ChromiumOptions,SessionPage,WebPage
-
-page = WebPage()
-page.get("https://www.ozon.ru/product/d-aromatizator-avtomobilnyy-cologne-1415420851/?oos_search=false")
-print(page.html)
-with open("test2.html", "w", encoding="utf-8") as f:
-    f.write(page.html)
